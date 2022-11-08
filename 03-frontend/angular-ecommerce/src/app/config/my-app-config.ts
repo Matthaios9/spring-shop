97de18860206fb96c58f11032c056d6077a3fb05
Matthaios9 <73692514+Matthaios9@users.noreply.github.com>
2022-11-08 11:04:03 +0100

    Complete Springboot Shop

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3wgd3g8vkxNG6Q5d7',
+        issuer: 'https://dev-81537373.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
